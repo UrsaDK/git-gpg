49ea9c1b03b1419a008111630776e73490b98636
Dmytro Konstantinov <ursa.dk@icloud.com>
2020-06-05 22:58:17 +0200

    Added a test for calling help on commands that incude a sub-command

--- spec/git-gpg_spec.cr
+++ spec/git-gpg_spec.cr
@@ -70,6 +70,12 @@ describe GitGPG do
       )
       $?.success?.should be_true
     end
+    it "is shown by help for a command with a sub-command" do
+      `#{git_gpg} help clean non-existent-file`.should start_with(
+        "Usage: #{GitGPG.name} clean [options] <file>"
+      )
+      $?.success?.should be_true
+    end
   end
 
   context "with invalid option" do
