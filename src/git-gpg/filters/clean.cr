d8778907b6a7a14ca4f8f07b0503e9c8ce2c569a
Dmytro Konstantinov <ursa.dk@icloud.com>
2020-06-05 22:50:54 +0200

    Implemented parser for the clean command

--- src/git-gpg/filters/clean.cr
+++ src/git-gpg/filters/clean.cr
@@ -4,8 +4,30 @@ module GitGPG
       extend self
 
       def main(args)
-        puts "Filter: #{File.basename(__FILE__, ".cr")}"
-        puts "Args: #{args}"
+        OptionParser.parse(args) do |parser|
+          parser.banner = <<-END_OF_BANNER
+          Usage: #{GitGPG.name} clean [options] <file>
+          Git clean filter used to encrypt file content\n
+          END_OF_BANNER
+
+          parser.on("-?", "--help", "Shows this help message") do
+            puts parser
+            exit
+          end
+
+          parser.separator <<-END_OF_SEPARATOR
+          \nThis filter accepts a single parameter which defines the path
+          to the file the filter is working on.
+          END_OF_SEPARATOR
+
+          parser.invalid_option do |option|
+            STDERR.puts "ERROR: #{option} is not a valid option"
+            puts "\n#{parser}" unless GitGPG.verbosity == Verbosity::Quiet
+            exit(1)
+          end
+        end
+
+        puts "--> #{args}"
       end
     end
   end
