ff66507bf67e348f63f764f90880516994384939
Dmytro Konstantinov <ursa.dk@icloud.com>
2020-06-16 13:42:15 +0200

    Added skeleton "clean" filter

--- src/git-gpg/filters/clean.cr
+++ src/git-gpg/filters/clean.cr
@@ -3,30 +3,23 @@ module GitGPG
     module Clean
       extend self
 
-      def main(args)
-        OptionParser.parse(args) do |parser|
-          parser.banner = <<-END_OF_BANNER
-          Usage: #{Attributes.name} clean [options] <file>
-          Git clean filter used to encrypt file content\n
-          END_OF_BANNER
+      def main
+        OptionParser.parser.banner = "#{parser_banner}\n"
+        OptionParser.parser.separator("\n#{parser_footer}")
+      end
 
-          parser.on("-?", "--help", "Shows this help message") do
-            puts parser
-            exit
-          end
+      private def parser_banner
+        <<-END_OF_BANNER
+        Usage: #{Attributes.name} clean [options] <file>
+        Git clean filter used to encrypt file content
+        END_OF_BANNER
+      end
 
-          parser.separator <<-END_OF_SEPARATOR
-          \nThis filter accepts a single parameter which defines the path
-          to the file the filter is working on.
-          END_OF_SEPARATOR
-
-          parser.invalid_option do |option|
-            raise GitGPG::Error.new("#{option} is not a valid option",
-                                    parser.to_s)
-          end
-        end
-
-        puts "--> #{args}"
+      private def parser_footer
+        <<-END_OF_FOOTER
+        This filter accepts a single parameter which defines the path
+        to the file the filter is working on.
+        END_OF_FOOTER
       end
     end
   end
